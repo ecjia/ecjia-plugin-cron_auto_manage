18be8b9bca3a3fdfa545f7d2e01d8bb1cae7a395
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-11 18:44:49 +0800

    更新计划任务插件新的实例化对象方法

--- cron_auto_manage.php
+++ cron_auto_manage.php
@@ -24,15 +24,20 @@ class plugin_cron_auto_manage {
         return RC_Api::api('cron', 'plugin_uninstall', $param);
     }
 
-    public static function adapter_instance($instance, $config) {
-        require_once RC_Plugin::plugin_dir_path(__FILE__) . 'cron_auto_manage.class.php';
-        return new cron_auto_manage($config);
-    }
+//     public static function adapter_instance($instance, $config) {
+//         require_once RC_Plugin::plugin_dir_path(__FILE__) . 'cron_auto_manage.class.php';
+//         return new cron_auto_manage($config);
+//     }
 
 }
 
+Ecjia_PluginManager::extend('cron_auto_manage', function() {
+    require_once RC_Plugin::plugin_dir_path(__FILE__) . 'cron_auto_manage.class.php';
+        return new cron_auto_manage();
+});
+
 RC_Plugin::register_activation_hook(__FILE__, array('plugin_cron_auto_manage', 'install'));
 RC_Plugin::register_deactivation_hook(__FILE__, array('plugin_cron_auto_manage', 'uninstall'));
-RC_Hook::add_filter('cron_factory_adapter_instance', array( 'plugin_cron_auto_manage', 'adapter_instance' ), 10, 2);
+// RC_Hook::add_filter('cron_factory_adapter_instance', array( 'plugin_cron_auto_manage', 'adapter_instance' ), 10, 2);
 
 // end
